8cfa365ed825422de32e46f55bf963afa7400fa2
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2023-10-22 19:21:26 -0400

    cleanup

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with, inputs
 from flask_sqlalchemy import SQLAlchemy
 
-# Start Server
+# Server
 app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
