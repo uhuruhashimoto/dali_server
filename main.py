b8d0b5545b4dfec8d852c115dc71d1ca5db67166
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2023-10-21 20:08:50 -0400

    get and put dali member by name

--- main.py
+++ main.py
@@ -8,22 +8,52 @@ api = Api(app)
 
 # Argument Parsing
 member_put_args = reqparse.RequestParser()
-member_put_args.add_argument("name", type=str, help="Name of the DALI member")
+# member_put_args.add_argument("name", type=str, help="Name of the DALI member is required", location='form', required=True)
+member_put_args.add_argument("year", type=str, help="Graduation year of the DALI member is required", location='form', required=True)
+member_put_args.add_argument("dev", type=bool, help="DALI member dev [true/false] is required", location='form', required=True)
+member_put_args.add_argument("des", type=bool, help="DALI member des [true/false] is required", location='form', required=True)
+member_put_args.add_argument("pm", type=bool, help="DALI member pm [true/false] is required", location='form', required=True)
+member_put_args.add_argument("core", type=bool, help="DALI member core [true/false] is required", location='form', required=True)
+member_put_args.add_argument("mentor", type=bool, help="DALI member mentor [true/false] is required", location='form', required=True)
+member_put_args.add_argument("major", type=str, help="Major of the DALI member is required", location='form', required=True)
+member_put_args.add_argument("minor", type=str, help="Minor of the DALI member is required", location='form', required=True)
+member_put_args.add_argument("birthday", type=str, help="Birthday [mm-dd] of the DALI member is required", location='form', required=True)
+member_put_args.add_argument("home", type=str, help="Home [city, state] of the DALI member is required", location='form', required=True)
+member_put_args.add_argument("quote", type=str, help="Quote is required", location='form', required=True)
+member_put_args.add_argument("favorite thing 1", type=str, help="Favorite thing 1 is required", location='form', required=True)
+member_put_args.add_argument("favorite thing 2", type=str, help="Favorite thing 2 is required", location='form', required=True)
+member_put_args.add_argument("favorite thing 3", type=str, help="Favorite thing 3 is required", location='form', required=True)
+member_put_args.add_argument("favorite dartmouth tradition", type=str, help="Favorite Dartmouth tradition is required", location='form', required=True)
+member_put_args.add_argument("fun fact", type=str, help="Fun fact is required", location='form', required=True)
+member_put_args.add_argument("picture", type=str, help="Picture url is required", location='form', required=True)
 
 # Data (placeholder)
 filename = "dali_social_media.json"
 f = open(filename)
-members = json.load(f)
+# Loads a list of dicts, each containing member info
+memberlist = json.load(f)
+members = {}
+# Translate this to a dict
+for member in memberlist:
+    if "name" in member:
+        whichname=member["name"]
+        del member["name"]
+        members[whichname] = member
+    else:
+        print("Error: no name found in json entry.")
 f.close()
 
 # Endpoints
 class DaliMember(Resource):
     def get(self, name: str):
-        return members
+        if name in members:
+            return members[name]
+        return {"message":"No member of name " + name}
 
     def put(self, name: str):
-        print(request.form)
-        return members
+        args = member_put_args.parse_args()
+        members[name] = args
+        return members[name]
 
 # Assemble API
 api.add_resource(DaliMember, "/dalimember/<string:name>")
--- test.py
+++ test.py
@@ -3,9 +3,28 @@ import requests
 BASE = "http://127.0.0.1:5000/"
 
 # Get Test
-get_response = requests.get(BASE + "dalimember/bob")
+get_response = requests.get(BASE + "dalimember/Amon Ferri")
 print(get_response.json())
 
-# Post Test
-put_response = requests.put(BASE + "dalimember/andy", {"major": "math"})
+# Put Test
+put_response = requests.put(BASE + "dalimember/Andy",
+                            {
+                                "year": "2023",
+                                "dev": True,
+                                "des": False,
+                                "pm": False,
+                                "core": False,
+                                "mentor": False,
+                                "major": "Computer Science",
+                                "minor": "Digital Art",
+                                "birthday": "05-06",
+                                "home": "New York, New York",
+                                "quote": "...",
+                                "favorite thing 1": "Fresh pineapple",
+                                "favorite thing 2": "Wintermint",
+                                "favorite thing 3": "Sunny days",
+                                "favorite dartmouth tradition": "Lobster bisque",
+                                "fun fact": "Whales are real",
+                                "picture": "https://api.typeform.com/responses/files/39f37747544d2be7038e60d7637f1f4e6d587f239c4bfbc1ecf3457ff05391d6/owlbear.png"
+                            })
 print(put_response.json())
